e7f01419c0b25681f162d923dd5f09d68d4a0fa1
xivk <ben@itinero.tech>
2019-02-04 15:09:18 +0100

    Finished upgrading things to new tile format.

--- test/Itinero.IO.Osm.Tiles.Test.Functional/Program.cs
+++ test/Itinero.IO.Osm.Tiles.Test.Functional/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Itinero.IO.Shape;
 using Itinero.LocalGeo;
@@ -21,11 +22,19 @@ namespace Itinero.IO.Osm.Tiles.Test.Functional
             routerDb.AddSupportedVehicle(Itinero.Osm.Vehicles.Vehicle.Car);
             
             // start loading tiles.
-            routerDb.LoadOsmDataFromTiles(new Box(51.179773424875634f, 4.5366668701171875f,
-                51.29885215199866f, 4.8017120361328125f));
+            routerDb.LoadOsmDataFromTiles(new Box(51.265271575597446f, 4.793086051940918f,
+                51.24195743492624f, 4.748368263244629f));
             
             // write as shapefile for testing.
             routerDb.WriteToShape("shapefile", routerDb.GetSupportedProfiles().ToArray());
+            
+            // calculate route.
+            var router = new Router(routerDb);
+            var route = router.Calculate(routerDb.GetSupportedProfile("car"),
+                new Coordinate(51.265271575597446f, 4.793086051940918f),
+                new Coordinate(51.24195743492624f, 4.748368263244629f));
+            File.WriteAllText("route.geojson", route.ToGeoJson());
+            
         }
     }
 }
